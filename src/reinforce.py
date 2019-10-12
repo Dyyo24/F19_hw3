b83a52e5eb5a53aa14c9915340c34cf034518fdd
Dyyo <35265978+Dyyo24@users.noreply.github.com>
2019-10-11 21:00:39 -0400

    Running script
    Reward stuck at negative, identifying issue

--- src/reinforce.py
+++ src/reinforce.py
@@ -9,6 +9,8 @@ from keras import losses
 from keras import Sequential
 from keras.layers import Dense
 
+#import torch
+#import torch.nn as nn
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -17,11 +19,12 @@ import matplotlib.pyplot as plt
 class Reinforce(object):
     # Implementation of the policy gradient method REINFORCE.
 
-    def __init__(self, model, lr):
+    def __init__(self, model, lr=5e-4):
         self.model = model
-        self.model.compile(loss=losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(learning_rate=0.001))
+        self.model.compile(loss=losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(learning_rate=lr))
         # TODO: Define any training operations and optimizers here, initialize
         #       your variables, or alternately compile your model here.
+
 
     def train(self, env, gamma=1.0):
         # Trains the model on a single episode using REINFORCE.
@@ -29,11 +32,29 @@ class Reinforce(object):
         #       method generate_episode() to generate training data.
 
         states, actions, rewards = self.generate_episode(env)
+        
         n = len(states)
         G = []
-        for i in range(n-1,-1,-1):  # counting backwards index  
-            G.append(np.sum([rewards[j]*np.power(gamma,j-i) for j in range(i,1,n)]))                
-        self.model.fit(np.array(states),-1*np.array(G))
+        
+        r = np.array(rewards)
+        G = np.zeros_like(r)
+        G[-1] = r[-1]
+        for i in range(n-2,-1,-1):
+            G[i] = G[i+1]*gamma + r[i]
+        
+       # for i in range(n-1,-1,-1):  # counting backwards index  
+       #     G.append(np.sum([rewards[j]*np.power(gamma,j-i) for j in range(i,1,n)])) 
+        
+        # one hot vector for G
+        in_shape = env.action_space.n
+        Ghot = np.zeros((n,in_shape))
+ #      print('hot vector shape: ',Ghot.shape)
+        for i in range(n):
+            Ghot[i,actions[i]] *= G[i]
+            
+    #    print('G',np.array(G).shape)
+   #     print('states',np.array(states).shape)              
+        self.model.fit(np.array(states),Ghot/n,verbose = 0)
 
 
     def generate_episode(self, env, render=False):
@@ -49,8 +70,8 @@ class Reinforce(object):
         done = False
         state = env.reset()
         while not done:
-            act_probs = self.model.predict(state)    # evaluate policy
-            act = np.random.choice(1, p = act_probs)
+            act_probs = self.model.predict(state.reshape(1,-1)).squeeze() # evaluate policy
+            act = np.random.choice(np.arange(act_probs.shape[0]), p = act_probs)
             next_state,r,done,_ = env.step(act)
             state = next_state.copy()
             states.append(next_state)
@@ -69,8 +90,8 @@ class Reinforce(object):
             # Check if the game is terminated
             while done == False:
                 # Take action and observe
-                act_probs = self.model.predict(state).tolist()[0]
-                act = np.random.choice(1, p = act_probs)
+                act_probs = self.model.predict(state.reshape(1,-1)).squeeze() # evaluate policy
+                act = np.random.choice(np.arange(act_probs.shape[0]), p = act_probs)
                 
                 next_state,r,done,_ = env.step(act)
                 state = next_state.copy()
@@ -105,7 +126,9 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def main(args):
+
+        
+#def main(args):
     # Parse command-line arguments.
 #    args = parse_arguments()
 #    num_episodes = args.num_episodes
@@ -113,28 +136,37 @@ def main(args):
 #    render = args.render
     
     # Create the environment.
-    env = gym.make('LunarLander-v2')
-    
-    # TODO: Create the model.
-    in_shape = env.observation_space.shape[0]
-    model = Sequential()       # the model that is trained for Q value estimator (Q hat)
-    model.add(Dense(16,kernel_initializer=initializers.VarianceScaling(scale=1.0,mode='fan_avg',distribution='uniform'),bias_initializer='zeros', activation='relu', input_shape=(in_shape,)))  # input: state and action
-    model.add(Dense(16,kernel_initializer=initializers.VarianceScaling(scale=1.0,mode='fan_avg',distribution='uniform'),bias_initializer='zeros',activation='relu'))
-    model.add(Dense(16,kernel_initializer=initializers.VarianceScaling(scale=1.0,mode='fan_avg',distribution='uniform'),bias_initializer='zeros',activation='relu'))
-    model.add(Dense(4,kernel_initializer=initializers.VarianceScaling(scale=1.0,mode='fan_avg',distribution='uniform'),bias_initializer='zeros', activation='softmax'))
+env = gym.make('LunarLander-v2')
+# TODO: Create the model.
+in_shape = env.observation_space.shape[0]
 
-    
-    # TODO: Train the model using REINFORCE and plot the learning curve.
-    reinforce_model = Reinforce(env)
-    r = 0
-    iteration = 0
-    while r < 200:
-        reinforce_model.train(env)
-        iteration += 1
-        if iteration % 200 == 0:
-            r = reinforce_model.test(env)
-            print('Reward: ',r)
-    
-    
-if __name__ == '__main__':
-    main(sys.argv)
+
+model = Sequential()       # the model that is trained for Q value estimator (Q hat)
+model.add(Dense(16,kernel_initializer=initializers.VarianceScaling(scale=1.0,mode='fan_avg',distribution='uniform'),bias_initializer='zeros', activation='relu', input_shape=(in_shape,)))  # input: state and action
+model.add(Dense(16,kernel_initializer=initializers.VarianceScaling(scale=1.0,mode='fan_avg',distribution='uniform'),bias_initializer='zeros',activation='relu'))
+model.add(Dense(16,kernel_initializer=initializers.VarianceScaling(scale=1.0,mode='fan_avg',distribution='uniform'),bias_initializer='zeros',activation='relu'))
+model.add(Dense(4,kernel_initializer=initializers.VarianceScaling(scale=1.0,mode='fan_avg',distribution='uniform'),bias_initializer='zeros', activation='softmax'))
+
+
+
+
+
+# TODO: Train the model using REINFORCE and plot the learning curve.
+reinforce_model = Reinforce(model)
+r = 0
+iteration = 0
+while r < 200:
+    reinforce_model.train(env)
+    iteration += 1
+    if iteration % 200 == 0:
+        r = reinforce_model.test(env)
+        print('------------------------')
+        print('Reward: ',r)
+        print('Iteration: ',iteration)
+
+# train on batches, or do normalization of reward
+        
+        
+
+#if __name__ == '__main__':
+#    main(sys.argv)
